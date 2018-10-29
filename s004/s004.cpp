6ae765cdc1a7d9e21d48f5a21f89c999242e4403
xmww_m <wwxm2015@gmail.com>
2018-10-29 15:25:34 +0800

    add s004

--- s004/s004.cpp
+++ s004/s004.cpp
@@ -9,68 +9,58 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> twoSum(vector<int>& nums, int target)
+    double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size)
     {
-        vector<int> ret;
-        for(int i = 0; i < nums.size(); i++)
+        if(0 == nums1Size)
         {
-           for(int j = i + 1; j < nums.size(); j++)
-           {
-               if((nums[i] + nums[j]) == target)
-               {
-
-                   ret.push_back(i);
-                   ret.push_back(j);
-                   return ret;
-               }
-           }
-        }
-        return ret;
-    }
-
-    vector<int> twoSum2(vector<int>& nums, int target)
-    {
-        vector<int> ret;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            for(int j = i + 1; j < nums.size(); j++)
+            if(nums2Size % 2)
             {
-                bool success = [](int a, int b, int target) { return (( a + b ) == target) ? true : false; }(nums[i], nums[j], target);
-                if(success)
-                {
-                    ret.push_back(i);
-                    ret.push_back(j);
-                    return ret;
-                }
+                return nums2[nums2Size / 2];
+            }else
+            {
+                return (nums2[nums2Size / 2] + nums2[nums2Size / 2 + 1]) / 2;
             }
         }
-        return ret;
+        if(0 == nums2Size)
+        {
+            if(nums1Size % 2)
+            {
+                return nums1[nums1Size / 2];
+            }else
+            {
+                return (nums1[nums1Size / 2] + nums1[nums1Size / 2 + 1]) / 2;
+            }
+        }
+        if(1 == nums1Size && 1 == nums2Size)
+        {
+
+        }
+
+
+        if(nums1[0] < nums2[1])
+        {
+
+        }else
+        {
+
+        }
+
+        if(nums1[nums1Size - 1] < nums2[nums2Size - 1])
+        {
+
+        }else
+        {
+
+        }
     }
 };
 
+/*
+ * nums1 = [1, 2]
+   nums2 = [3, 4]
+ */
+
 int main()
 {
-    //2, 7, 11, 15
-    Solution s;
-    vector<int> nums;
-    nums.push_back(2);
-    nums.push_back(7);
-    nums.push_back(11);
-    nums.push_back(15);
-    auto ret = s.twoSum2(nums, 9);
-    for(auto it = ret.begin(); it !=  ret.end(); it ++)
-    {
-        cout << *it << endl;
-    }
-
-    vector<int> nums2;
-    nums2.push_back(3);
-    nums2.push_back(2);
-    nums2.push_back(4);
-    auto ret2 = s.twoSum2(nums2, 6);
-    for(auto it = ret2.begin(); it !=  ret2.end(); it ++)
-    {
-        cout << *it << endl;
-    }
     return 0;
 }
