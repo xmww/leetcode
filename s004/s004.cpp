f5c1401ff9cbd0cd7f5c89f177c9e05484736470
xmww_m <wwxm2015@gmail.com>
2018-10-29 15:41:29 +0800

    add s004

--- s004/s004.cpp
+++ s004/s004.cpp
@@ -11,6 +11,7 @@ class Solution
 public:
     double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size)
     {
+        printf("1:%d, 2:%d\n", nums1Size, nums2Size);
         if(0 == nums1Size)
         {
             if(nums2Size % 2)
@@ -18,7 +19,7 @@ public:
                 return nums2[nums2Size / 2];
             }else
             {
-                return (nums2[nums2Size / 2] + nums2[nums2Size / 2 + 1]) / 2;
+                return (nums2[nums2Size / 2] + nums2[nums2Size / 2 - 1]) / 2.0;
             }
         }
         if(0 == nums2Size)
@@ -28,30 +29,34 @@ public:
                 return nums1[nums1Size / 2];
             }else
             {
-                return (nums1[nums1Size / 2] + nums1[nums1Size / 2 + 1]) / 2;
+                return (nums1[nums1Size / 2] + nums1[nums1Size / 2 - 1]) / 2.0;
             }
         }
         if(1 == nums1Size && 1 == nums2Size)
         {
-
+            return (nums1[0] + nums2[0]) / 2.0;
         }
 
 
-        if(nums1[0] < nums2[1])
+        if(nums1[0] < nums2[0])
         {
-
+            nums1 ++;
+            nums1Size --;
         }else
         {
-
+            nums2 ++;
+            nums2Size --;
         }
 
         if(nums1[nums1Size - 1] < nums2[nums2Size - 1])
         {
-
+            nums2Size --;
         }else
         {
-
+            nums1Size --;
         }
+
+        return findMedianSortedArrays(nums1, nums1Size, nums2, nums2Size);
     }
 };
 
@@ -62,5 +67,30 @@ public:
 
 int main()
 {
+    Solution s;
+    {
+
+        int nums1[] = {1, 2};
+        int nums2[] = {3, 4};
+        printf("ret:%f\n", s.findMedianSortedArrays(nums1, 2, nums2, 2));
+    }
+
+    {
+        int nums1[] = {1};
+        int nums2[] = {3, 4};
+        printf("ret:%f\n", s.findMedianSortedArrays(nums1, 1, nums2, 2));
+    }
+
+    {
+        int nums1[] = {1};
+        int nums2[] = {3};
+        printf("ret:%f\n", s.findMedianSortedArrays(nums1, 1, nums2, 1));
+    }
+
+    {
+        int nums1[] = {1, 2, 3};
+        int nums2[] = {4};
+        printf("ret:%f\n", s.findMedianSortedArrays(nums1, 3, nums2, 1));
+    }
     return 0;
 }
